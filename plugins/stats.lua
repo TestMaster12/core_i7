9799400aafcf4928b0b87613f1722dc61b0b1f93
shahpri <m.shahpari96@gmail.com>
2016-02-09 19:31:42 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'king' then -- Put everything you like :)
+  if matches[1]:lower() == 'shield' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /creedbot ")
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "king" then -- Put everything you like :)
+    if matches[2] == "shield" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,13 +141,13 @@ return {
     "^[!/#$]([Ss]tats)$",
     "^[!/#$]([Ss]tatslist)$",
     "^[!/#$]([Ss]tats) (group) (%d+)",
-    "^[!/#$]([Ss]tats) (king)",-- Put everything you like :)
-		"^[!/#$]([Kk]ing)",
-		"^[!/#$]([Ss]tats)$",
+    "^[!/#$]([Ss]tats) ([sS][Hh][Ii][Ee][Ll][Dd])",-- Put everything you like :)
+		"^[!/#$]([sS][Hh][Ii][Ee][Ll][Dd])",
+		"^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (king)",-- Put everything you like :)
-		"^([Kk]ing)"-- Put everything you like :)-- Put everything you like :)
+    "^([Ss]tats) ([sS][Hh][Ii][Ee][Ll][Dd])",-- Put everything you like :)
+		"^([sS][Hh][Ii][Ee][Ll][Dd])"-- Put everything you like :)-- Put everything you like :)
     }, 
   run = run
 }
