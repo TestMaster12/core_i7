c71539c05c1f0848139ff61503e08f8017f7d7a9
shahpri <m.shahpari96@gmail.com>
2016-02-14 22:01:50 +0330

    Update banhammer.lua

--- plugins/banhammer.lua
+++ plugins/banhammer.lua
@@ -130,7 +130,7 @@ local function run(msg, matches)
     elseif matches[1]:lower() == 'id' then
       local name = user_print_name(msg.from) .. ' (user#id' .. msg.from.id .. ')'
       savelog(msg.to.id, name.." ["..msg.from.id.."] used /id ")
-      return "Your ID:\n "..msg.from.id ..   "Group ID:\n "..msg.to.id
+      return "Your ID: "..msg.from.id ..   "Group ID: "..msg.to.id
     end
   end
   local receiver = get_receiver(msg)
