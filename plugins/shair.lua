1601ae315312564ce605135d8371301725bea822
Lenovocorei7 <m.r.mehrparvar@live.com>
2016-02-16 00:43:58 +0330

    Create shair.lua

---
+++ plugins/shair.lua
@@ -0,0 +1,21 @@
+--shared by @blackhatchannel
+
+do
+function run(msg, matches)
+  if not is_sudo(msg) then
+    return "Only sudoers can give bot account "
+    end
+function run(msg, matches)
+send_contact(get_receiver(msg), "Your Bot Phone Number", "fri name", "Las name", ok_cb, false)
+end
+
+return {
+patterns = {
+"^!share$"
+
+},
+run = run
+}
+
+end
+--shared by @blackhatchannel
