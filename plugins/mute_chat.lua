59554f54fbf2885832f8ccf24bdcef8801105ba5
Lenovocorei7 <m.r.mehrparvar@live.com>
2016-02-18 17:20:53 +0330

    Update mute_chat.lua

--- plugins/mute_chat.lua
+++ plugins/mute_chat.lua
@@ -1,36 +1,27 @@
-
-antichat = {}-- An empty table for solving multiple kicking problem
-
-do
 local function run(msg, matches)
-  if is_momod(msg) then -- Ignore mods,owner,admins
-    return
-  end
-  local data = load_data(_config.moderation.data)
-  if data[tostring(msg.to.id)]['settings']['mute_chat'] then
-    if data[tostring(msg.to.id)]['settings']['mute_chat'] == 'yes' then
-      if antichat[msg.from.id] == true then 
+    if is_owner(msg) then
         return
-      end
-      send_large_msg("chat#id".. msg.to.id , "chat is not allowed here")
-      local name = user_print_name(msg.from)
-      savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (chat was locked) ")
-      chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id,ok_cb,false)
-		  antichat[msg.from.id] = true
-      return
     end
-  end
-  return
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_chat'] then
+                lock_gif = data[tostring(msg.to.id)]['settings']['lock_chat']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_chat == "yes" then
+        send_large_msg(chat, 'chat is not allowed in this chat!')
+		      savelog(msg.to.id," ["..msg.from.id.."] kicked user [chat was locked] !")-- Save to logs
+        chat_del_user(chat, user, ok_cb, true)
+    end
 end
-local function cron()
-  antichat = {} -- Clear antichat table 
-end
+ 
 return {
   patterns = {
-    "(.+)"
-    },
-  run = run,
-	cron = cron
+     "(.+)"
+  },
+  run = run
 }
-
-end
