952549296a78e652ae3cdf559dbeb63c038a5739
Lenovocorei7 <m.r.mehrparvar@live.com>
2016-02-18 16:43:02 +0330

    Update lock_emoji.lua

--- plugins/lock_emoji.lua
+++ plugins/lock_emoji.lua
@@ -13,7 +13,7 @@ local function run(msg, matches)
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
     if lock_gif == "yes" then
-        send_large_msg(chat, 'Gif is not allowed in this chat!')
+        send_large_msg(chat, 'emoji is not allowed in this chat!')
 		      savelog(msg.to.id,"@blackhatchannel ["..msg.from.id.."] kicked user [gif was locked] !")-- Save to logs
         chat_del_user(chat, user, ok_cb, true)
     end
