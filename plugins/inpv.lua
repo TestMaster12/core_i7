ee3a96571855c9eacbec6dccbdbd569bce4340fe
Lenovocorei7 <m.r.mehrparvar@live.com>
2016-02-21 10:57:37 +0330

    Update inpv.lua

--- plugins/inpv.lua
+++ plugins/inpv.lua
@@ -1,16 +1,22 @@
-local function run(msg)
-    if msg.text == "عشقم" then
-        	return "بله عزیزم ؟"
-        end
-end
-
-return {
-	description = "Chat With Robot Server", 
-	usage = "chat with robot",
-	patterns = {
-	       	"^(عشقم)$",
-	       			}, 
-	run = run,
-    --privileged = true,
-	pre_process = pre_process
+local function run(msg, matches)
+ if matches[1]:lower() == 'id' or matches[1]:lower() == 'res' then
+    if msg.to.type == "user" then
+     return "Bot ID: "..msg.to.id.. "Your ID: "..msg.from.id
+    end
+    if type(msg.reply_id) ~= "nil" then
+      local name = user_print_name(msg.from)
+        savelog(msg.to.id, name.." ["..msg.from.id.."] used /id ")
+      id = get_message(msg.reply_id,get_message_callback_id, false)
+    elseif matches[1]:lower() == 'id' then
+      local name = user_print_name(msg.from) .. ' (user#id' .. msg.from.id .. ')'
+      savelog(msg.to.id, name.." ["..msg.from.id.."] used /id ")
+      return salam
+    end
+  end
+  return {
+  	  patterns = {
+  	  	 "^[!/$#]([Ii]d)$"
+  	  	 },
+  run = run,
+  pre_process = pre_process
 }
