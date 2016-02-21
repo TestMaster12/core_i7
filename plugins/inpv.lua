b889abe28362ac5d562f7600e6d338501af87e38
Lenovocorei7 <m.r.mehrparvar@live.com>
2016-02-21 10:24:07 +0330

    Update inpv.lua

--- plugins/inpv.lua
+++ plugins/inpv.lua
@@ -1,12 +1,10 @@
-local function run(msg, matches)
- if matches[1]:lower() == '(.+)'
+local function run(msg)
     if msg.to.type == "user" then
-     return "\n\nYour ID: "..msg.from.id.. \n\n dont talk to my in pv
+     return "\n\nYour ID: "..msg.from.id
     end
     return {
-     patterns = {
-                  "(.+)",
-                },
-                 run = run,
-  pre_process = pre_process
+  patterns = {
+     "(.+)"
+  },
+  run = run
 }
