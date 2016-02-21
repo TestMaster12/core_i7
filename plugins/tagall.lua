1a2ec680201406c005865fc682d5ded9c2c55a25
Lenovocorei7 <m.r.mehrparvar@live.com>
2016-02-21 11:06:30 +0330

    Update tagall.lua

--- plugins/tagall.lua
+++ plugins/tagall.lua
@@ -1,27 +1,25 @@
 --Tag ppl with username and a msg after it
-
 local function tagall(cb_extra, success, result)
     local receiver = cb_extra.receiver
     local chat_id = "chat#id"..result.id
     local text = ''
+    local i = 0 + 1
     for k,v in pairs(result.members) do
         if v.username then
-			text = text.."@"..v.username.."\n"
+			text = text..i.."- @"..v.username.."\n"
 		end
     end
 	text = text.."\n"..cb_extra.msg_text
 	send_large_msg(receiver, text)
 end
 local function run(msg, matches)
-	if matches[1] == 'tagall' then
     local receiver = get_receiver(msg)
 	if not is_owner(msg) then 
 		return "For owner only !"
 	end
-	if matches[2] then
+	if matches[1] then
 		chat_info(receiver, tagall, {receiver = receiver,msg_text = matches[1]})
 	end
-end
 	return
 end
 
@@ -29,11 +27,15 @@ end
 return {
   description = "Will tag all ppl with a msg.",
   usage = {
-    "tagall: Tag All Users And Show Your Message.",
+    "/tagall [msg]."
   },
   patterns = {
-    "^(tagall) +(.+)$",
-    "^[!/#$]tagall +(.+)$"
+    "^[!/$#]tagall +(.+)$",
+    "^tagall +(.+)$"
   },
   run = run
 }
+--Made By @ImanDaneshi
+    --https://github.com/ThisIsArman
+    --Telegram.me/ThisIsArman
+--edit by amir :/
